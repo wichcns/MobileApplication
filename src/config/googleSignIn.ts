f3db34db2771d83715a5f11cabde73fd28c60d7c
wichcns <wich_sirawich@hotmail.com>
2026-09-02 16:58:08 +0700

    Prepare iOS 4.2.0 for TestFlight

--- src/config/googleSignIn.ts
+++ src/config/googleSignIn.ts
@@ -1,11 +1,18 @@
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { Platform } from 'react-native';
 
-const GOOGLE_WEB_CLIENT =
+const IOS_GOOGLE_WEB_CLIENT =
+  '458026609639-ttvbs5d5kihmn2nv01lsgs2mdat1e1pt.apps.googleusercontent.com';
+
+const ANDROID_GOOGLE_WEB_CLIENT =
   '909128229498-ggqlvh72f58o98a5ieb99nnjcg204kin.apps.googleusercontent.com';
 
 export const configureGoogleSignIn = () => {
   GoogleSignin.configure({
-    webClientId: GOOGLE_WEB_CLIENT,
+    webClientId:
+      Platform.OS === 'ios'
+        ? IOS_GOOGLE_WEB_CLIENT
+        : ANDROID_GOOGLE_WEB_CLIENT,
     forceCodeForRefreshToken: true,
   });
 };
